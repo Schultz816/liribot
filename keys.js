bc086da6de1c5e7cee69cbeedfea26861ea8b369
Schultz816 <Corey.w.schultz@gmail.com>
2017-04-23 21:14:35 -0500

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('The bot is starting');
+
+var Twit = require('twit');
+
+var T = new Twit({
+	consumer_key: "2oknngjx3aS8Wahfk2PoXIUpJ",
+	consumer_secret: "kXwAl1621XHRQC0ej4EeQ9KvPqDr2yDUB4nLTCCYlK7VEI3ufP";
+	access_token: "852999996253384704-s5NT1TxxnL8jjM1vtGtyvrjwoppgZeJ";
+	access_token_secret: "pQgXus8ayKBUQr2DtpS94Qqq86X0Kpn90n149AGoXnLjt"; 
+})
